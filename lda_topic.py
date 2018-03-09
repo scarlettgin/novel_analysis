f714b70e7fe90b406ce9db350de0db560ef53f3d
ma.shuangjian <ma.shuangjian@immomo.com>
2018-03-09 19:00:40 +0800

    init

--- lda_topic.py
+++ lda_topic.py
@@ -4,6 +4,8 @@ import numpy as np
 from basic import *
 from sklearn.feature_extraction.text import CountVectorizer
 import lda
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # http://blog.csdn.net/real_myth/article/details/51239847
@@ -29,7 +31,8 @@ def lda_train(weight, vectorizer):
         print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
     doc_topic = model.doc_topic_
-    print(doc_topic)
+    print(doc_topic, type(doc_topic))
+    plot_topic(doc_topic)
     for i in range(doc_num):
         print("{} (top topic: {})".format(titles[i], np.argsort(doc_topic[i])[:-4:-1]))
 
@@ -39,6 +42,19 @@ def main():
     chapters = MyChapters(chapter_list)
     weight, vectorizer = get_lda_input(chapters)
     lda_train(weight, vectorizer)
+
+
+def plot_topic(doc_topic):
+    # https://www.jianshu.com/p/363bbf6ec335
+    f, ax = plt.subplots(figsize=(10, 4))
+    cmap = sns.cubehelix_palette(start=1, rot=3, gamma=0.8, as_cmap=True)
+    sns.heatmap(doc_topic, cmap=cmap, linewidths=0.05, ax=ax)
+    ax.set_title('proportion per topic in every chapter')
+    ax.set_xlabel('topic')
+    ax.set_ylabel('chapter')
+    # plt.show()
+
+    f.savefig('data/topic_heatmap.jpg', bbox_inches='tight')
 
 
 if __name__ == '__main__':
